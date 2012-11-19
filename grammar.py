967f634126a939e1dc199a05fb23bd5352db5bb8
Christian Oudard <christian.oudard@gmail.com>
2012-11-19 14:29:31 -0800

    Added grammar information for Vr affix.

--- grammar.py
+++ grammar.py
@@ -1,5 +1,6 @@
-designations = ('FML', 'IFL')
-
+functions = ('STA', 'DYN', 'MNF', 'DSC')
+patterns = ('P1', 'P2', 'P3')
+stems = ('S1', 'S2', 'S3')
 configurations = (
     'UNI', 'DPX', 'DCT', 'AGG', 'SEG', 'CPN', 'COH', 'CST', 'MLT',
 )
@@ -7,6 +8,19 @@ affiliations = ('CSL', 'ASO', 'VAR', 'COA')
 perspectives = ('M', 'U', 'N', 'A')
 extensions = ('DEL', 'PRX', 'ICP', 'TRM', 'DPL', 'GRA')
 essences = ('NRM', 'RPV')
+designations = ('FML', 'IFL')
+
+def lines_to_tables(lines, keys):
+    # Read each line, split multiple affixes, and create 
+    # bidirectional dictionaries.
+    table = {}
+    table_reverse = {}
+    for affixes, key in zip(lines, keys):
+        affixes = affixes.split('|')
+        table[key] = tuple(sorted(affixes))
+        for affix in affixes:
+            table_reverse[affix] = key
+    return table, table_reverse
 
 def gen_ca_tables():
     keys = [
@@ -20,17 +34,24 @@ def gen_ca_tables():
 
     with open('ca_table.dat') as f:
         lines = f.read().splitlines()
-
     assert len(lines) == len(keys)
     assert len(set(lines)) == len(lines)
 
-    ca_table = {}
-    ca_table_reverse = {}
-    for affixes, key in zip(lines, keys):
-        affixes = affixes.split('|')
-        ca_table[key] = tuple(sorted(affixes))
-        for affix in affixes:
-            ca_table_reverse[affix] = key
-    return ca_table, ca_table_reverse
-
+    return lines_to_tables(lines, keys)
 ca_table, ca_table_reverse = gen_ca_tables()
+
+def gen_vr_tables():
+    keys = [
+        (fu, pa, st)
+        for fu in functions
+        for pa in patterns
+        for st in stems
+    ]
+
+    with open('vr_table.dat') as f:
+        lines = f.read().splitlines()
+    assert len(lines) == len(keys)
+    assert len(set(lines)) == len(lines)
+
+    return lines_to_tables(lines, keys)
+vr_table, vr_table_reverse = gen_vr_tables()
