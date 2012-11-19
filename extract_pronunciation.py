c3a5cd0903bd82a0275f5984ad5ef176d53f8bc8
Christian Oudard <christian.oudard@gmail.com>
2012-11-18 16:55:29 -0800

    Added a script to extract pronunciation guides from ithkuil website html.

---
+++ extract_pronunciation.py
@@ -0,0 +1,67 @@
+# coding: utf8
+
+import sys
+import os
+import re
+from collections import defaultdict
+from pyquery import PyQuery as pq
+
+def unwrap(t):
+    contents = pq(t).html()
+    if contents is None:
+        pq(t).remove()
+    else:
+        pq(t).replaceWith(contents)
+
+def extract(d):
+    # Remove redundant strong tags.
+    for t in reversed(d('strong strong')):
+        unwrap(t)
+
+    for e in d('strong'):
+        for piece in pq(e).text().split('/'):
+            yield piece, e
+
+alphabet_normal = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_ithkuil_special = 'âçêëîôöûüčļňřšţżž'
+
+def clean(text):
+    text = text.strip()
+    if len(re.sub('\W', '', text)) <= 3:
+        return ''
+    text = text.replace('\xa0', ' ') # Replace non-breaking spaces.
+    text = re.sub('[ ]+', ' ', text)
+    if '\n' in text:
+        return ''
+    if re.search('[0-9]', text):
+        return ''
+    if re.search('(^| )-', text):
+        return ''
+    if re.search('-($| )', text):
+        return ''
+    if not any(c in text for c in alphabet_ithkuil_special):
+        return ''
+    return text
+
+if __name__ == '__main__':
+    html_files = [
+        f for f in os.listdir(os.getcwd())
+        if f.endswith('.html')
+    ]
+    cleaned_to_original = defaultdict(list)
+    for filename in html_files:
+        # The filename keyword doesn't work for some reason?
+        #d = pq(filename=filename)
+
+        html = open(filename).read()
+        d = pq(html)
+        for text, tag in extract(d):
+            cleaned = clean(text)
+            if cleaned:
+                cleaned_to_original[cleaned].append(str(pq(tag)))
+
+    for cleaned in sorted(cleaned_to_original.keys()):
+        originals = cleaned_to_original[cleaned]
+        print(cleaned)
+        if len(originals) > 2 or originals[0] != cleaned:
+            print(originals)
---
+++ test_extract_pronunciation.py
@@ -0,0 +1,42 @@
+# coding: utf8
+
+import unittest
+from unittest import TestCase
+
+from pyquery import PyQuery as pq
+from extract_pronunciation import extract, clean, unwrap
+
+class ExtractTest(TestCase):
+    test_cases = [
+        (
+            '<strong><img src="images/5-10-33c.jpg" width="133" eight="46" /><br />\nAr-ryigrawilei&#355;rar&nbsp;  eglul&ocirc;n. </strong><span class="style25">&rarr; </span><strong>&Ccedil;tar-rya&nbsp; <strong>ei<strong><strong>r</strong></strong>&#355;</strong> <strong>&nbsp;</strong>igralar&nbsp;  eglul&ocirc;n.</strong>',
+            [
+                'Ar-ryigrawileiţrar\xa0  eglulôn.',
+                'Çtar-rya\xa0 eirţ \xa0igralar\xa0  eglulôn.',
+            ],
+        ),
+        (
+            '<strong>ea<strong></strong></strong>',
+            ['ea'],
+        ),
+    ]
+    def test_extract(self):
+        for html, sentences in self.test_cases:
+            self.assertEqual(
+                list(extract(pq(html))),
+                sentences,
+            )
+
+
+class UnwrapTest(TestCase):
+    def test_unwrap(self):
+        before = '<p>one two <strong>three <em>four</em></strong></p>'
+        after = '<p>one two three <em>four</em></p>'
+        d = pq(before)
+        t = d('strong')
+        unwrap(t)
+        self.assertEqual(str(d), after)
+
+
+if __name__ == '__main__':
+    unittest.main()
