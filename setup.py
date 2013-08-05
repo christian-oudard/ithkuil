f5251b4bea7c7320329aca64c61ff4ffb0e82244
Christian Oudard <christian.oudard@gmail.com>
2013-08-05 05:03:37 -0700

    Added a simple setup.py. Removed an unused svg file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ithkuil',
+    version='0.0',
+    description='',
+    author='Christian Oudard',
+    author_email='christian.oudard@gmail.com',
+    url='',
+    packages=['ithkuil'],
+)
