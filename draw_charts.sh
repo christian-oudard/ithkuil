3a81e5697182a7e21929b15f528d3cec5dbf2e84
Christian Oudard <christian.oudard@gmail.com>
2013-08-19 10:11:43 -0700

    Add .gif export to chart drawing script.

--- draw_charts.sh
+++ draw_charts.sh
@@ -2,6 +2,9 @@
 
 for file in charts/*.py; do
     base=${file/%.py/}
-    python $file > ${base}.svg && \
-    inkscape ${base}.svg --export-png=${base}.png
+    python $file > $base.svg && \
+    inkscape $base.svg --export-png=$base.png
+    convert $base.png $base.gif
+    pngcrush $base.png $base.crushed.png
+    mv $base.crushed.png $base.png
 done
