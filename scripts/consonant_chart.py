57becdfce3006cf6f71ec4e7c93bd389f2369c6d
Christian Oudard <christian.oudard@gmail.com>
2013-08-18 20:50:52 -0700

    Polish consonant chart.

--- scripts/consonant_chart.py
+++ scripts/consonant_chart.py
@@ -1,34 +1,31 @@
 from canoepaddle import Pen
 from canoepaddle.mode import StrokeOutlineMode
 
-from ithkuil.writing.common import TOP, BOTTOM
 from ithkuil.writing.consonant import consonants
 from ithkuil.writing.typeset import draw_letter, typeset
 from ithkuil.phonology import convert_ascii_to_html
 import ithkuil.writing.side_ending as se
 import ithkuil.writing.bottom_ending as be
 
-
 papers = []
-import ithkuil.writing.consonant as cons
-consonants = [cons.P, cons.B]
 for consonant_class in consonants:
     letter = consonant_class(se.Normal, be.Normal)
     letter_paper = draw_letter(
         letter,
         mode=StrokeOutlineMode(1.0, 0.1, '#c60416', '#3b0006'),
-        fixed_width=20.0,
+        fixed_width=25,
         show_template=True,
     )
-    letter_paper.translate((3, 0), bounds=False)
+    letter_paper.translate((5, 0), bounds=False)
 
     p = Pen()
     p.move_to((-5, 0))
     p.text(
         convert_ascii_to_html(consonant_class.pronunciation),
-        TOP - BOTTOM,
+        8.0,
         'Caudex',
         '#233042',
+        centered=True,
     )
     letter_paper.merge(p.paper)
     papers.append(letter_paper)
