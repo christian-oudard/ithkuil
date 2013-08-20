88db7e01b8ab95602c8f79b40c96c15f97386b82
Christian Oudard <christian.oudard@gmail.com>
2013-08-19 18:56:51 -0700

    Change hook() to go off of a middle "bone".

--- ithkuil/writing/util.py
+++ ithkuil/writing/util.py
@@ -7,7 +7,7 @@ from canoepaddle.heading import Angle
 
 
 # TODO: make distance central, not on the inside edge.
-def hook(pen, slant_angle, arc_angle, distance):
+def hook(pen, slant_angle, arc_angle, distance, adjust_inside=0, adjust_outside=0):
     """
     Draw a hook shape.
 
@@ -24,6 +24,9 @@ def hook(pen, slant_angle, arc_angle, distance):
     then the hook curves to the right instead.
 
     `distance` is the arc length along the inside arc.
+
+    `sharp_angle` adjusts the "sharpness" i.e. acuteness of the angle of the
+    hook tip. Negative angles make it "blunter".
     """
     slant_angle = Angle(slant_angle)
     arc_angle = Angle(arc_angle)
@@ -31,10 +34,11 @@ def hook(pen, slant_angle, arc_angle, distance):
     old_mode = pen.mode
     hook_width = old_mode.width
 
-    # The pen starts at the inside corner of the hook, facing along the
-    # base of the hook.
+
+    # The pen starts at the "middle" of the hook, a point along the base that
+    # is in the middle of the inside and outside bounding circles.
     base_heading = pen.heading
-    inside_corner = pen.position
+    base_middle = pen.position
 
     # Calculate the radius.
     circumference = distance / (arc_angle.theta / 360)
@@ -44,14 +48,13 @@ def hook(pen, slant_angle, arc_angle, distance):
     # important points.
     pen.turn_left(slant_angle)
     pen.arc_left(arc_angle, radius)
-    inside_arc = pen.last_segment()
+    center_arc = pen.last_segment()
     pen.undo()
 
-    tip = inside_arc.b
-    inside_center = inside_arc.center
-    inside_end_heading = inside_arc.end_heading
+    tip = center_arc.b
+    center = center_arc.center
 
-    # Calculate the outside corner position. The outside corner is located
+    # Calculate the inside and outside corner position. The outside corner is located
     # along the base line, intersecting the outer circle. The outer circle is
     # concentric to the inner arc's circle, but with radius larger by the
     # hook_width.
@@ -63,46 +66,57 @@ def hook(pen, slant_angle, arc_angle, distance):
 
     if switch_corners:
         v_base = vec.neg(v_base)
-    points = intersect_circle_line(
-        inside_arc.center,
-        abs(inside_arc.radius) + hook_width,
-        inside_corner,
-        vec.add(inside_corner, v_base),
-    )
-    # Take the intersection point that is "forward" from the inside corner.
-    points = [
-        p for p in points
-        if vec.dot(
-            vec.vfrom(inside_corner, p),
-            v_base,
-        ) > 0
-    ]
-    assert len(points) == 1
-    outside_corner = points[0]
 
-    if switch_corners:
-        # If the corners are switched, we actually started the hook at the
-        # outside corner, so we need to adjust.
-        v_base_width = vec.vfrom(outside_corner, inside_corner)
-        inside_corner = vec.add(inside_corner, v_base_width)
-        outside_corner = vec.add(outside_corner, v_base_width)
-        tip = vec.add(tip, v_base_width)
-        inside_center = vec.add(inside_center, v_base_width)
+    base_right = vec.add(base_middle, v_base)
+
+    points_inside = intersect_circle_line(
+        center, abs(center_arc.radius) - (hook_width / 2),
+        base_middle, base_right,
+    )
+    points_outside = intersect_circle_line(
+        center, abs(center_arc.radius) + (hook_width / 2),
+        base_middle, base_right,
+    )
+
+    # Take the intersection point that is the most "forward" along the base
+    # from the inside corner.
+    def forwardness(p):
+        v_p = vec.vfrom(base_middle, p)
+        return vec.dot(v_p, v_base),
+
+    inside_corner = max(points_inside, key=forwardness)
+    outside_corner = max(points_outside, key=forwardness)
+
+    # Adjust for having started in the middle of the hook. Move to where the
+    # corner of the hook is at the pen start position.
+    if not switch_corners:
+        offset = vec.vfrom(inside_corner, base_middle)
+    else:
+        offset = vec.vfrom(outside_corner, base_middle)
+    inside_corner = vec.add(inside_corner, offset)
+    outside_corner = vec.add(outside_corner, offset)
+    tip = vec.add(tip, offset)
+    center = vec.add(center, offset)
 
     # Draw the hook.
     pen.set_mode(old_mode.outliner_mode())
     # Base.
     pen.move_to(inside_corner)
     pen.line_to(outside_corner)
-    # Outer arc, tangent to outer circle.
-    pen.turn_toward(inside_center)
+    # Outer arc.
+    pen.turn_toward(center)
     if arc_angle > 0:
-        pen.turn_right(90)
+        pen.turn_right(90 + adjust_inside)
     else:
-        pen.turn_left(90)
+        pen.turn_left(90 + adjust_inside)
     pen.arc_to(tip)
     # Inner arc.
-    pen.turn_to(inside_end_heading + 180)
+    pen.turn_to(center_arc.end_heading + 180)
+    if arc_angle > 0:
+        pen.turn_right(adjust_outside)
+    else:
+        pen.turn_left(adjust_outside)
+
     pen.arc_to(inside_corner)
 
     pen.set_mode(old_mode)
--- scripts/hook_test.py
+++ scripts/hook_test.py
@@ -11,39 +11,41 @@ def get_pen():
     p.turn_to(0)
     return p
 
+def draw(
+    pen,
+    slant_angle,
+    arc_angle,
+    distance,
+    adjust_inside,
+    adjust_outside,
+):
+    old_pos = pen.position
+    old_heading = pen.heading
+    hook(pen, slant_angle, arc_angle, distance)
+    pen.move_to(old_pos)
+    pen.turn_to(old_heading)
+    hook(
+        pen, slant_angle, arc_angle, distance, adjust_inside=adjust_inside,
+        adjust_outside=adjust_outside,
+    )
+
 papers = []
 
-p = get_pen()
-hook(p, 45, 90, 5.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, 120, 75, 4.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, 60, -75, 4.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, 135, -90, 5.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, -45, -90, 5.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, -120, -75, 4.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, -60, 75, 4.0)
-papers.append(p.paper)
-
-p = get_pen()
-hook(p, -135, 90, 5.0)
-papers.append(p.paper)
+distance = 5.0
+for slant_angle in [60, -60, 120, -120]:
+    for arc_angle in [75, -75]:
+        for adjust_inside in [0, 30]:
+            for adjust_outside in [0, 30]:
+                pen = get_pen()
+                draw(
+                    pen,
+                    slant_angle,
+                    arc_angle,
+                    distance,
+                    adjust_inside,
+                    adjust_outside,
+                )
+                papers.append(pen.paper)
 
 page = typeset(
     papers,
